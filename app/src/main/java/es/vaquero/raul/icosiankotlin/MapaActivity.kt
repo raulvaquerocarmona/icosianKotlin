4429c763c0d21cfeb6cb7f4a795fd758414d1bca
Raulvaquerocarmona <raulvaquero.alum@insestatut.cat>
2022-04-05 19:09:05 +0200

    Acabada mapaactivity

--- app/src/main/java/es/vaquero/raul/icosiankotlin/MapaActivity.kt
+++ app/src/main/java/es/vaquero/raul/icosiankotlin/MapaActivity.kt
@@ -2,10 +2,26 @@ package es.vaquero.raul.icosiankotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapaActivity : AppCompatActivity() {
+class MapaActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
