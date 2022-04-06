e33b2a7fd34ed1fbb6058542e3b8f1eb6e88d0df
RaulAnton~ <raulanton.alum@insestatut.cat~>
2022-04-06 19:37:02 +0200

    He cambiado algunas cosas, el activity_main.xml con los botones y el MainActivity.kt con las funciones para iniciar sesion

--- app/src/main/java/es/vaquero/raul/icosiankotlin/MainActivity.kt
+++ app/src/main/java/es/vaquero/raul/icosiankotlin/MainActivity.kt
@@ -1,11 +1,78 @@
 package es.vaquero.raul.icosiankotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import es.vaquero.raul.icosiankotlin.databinding.ActivityMainBinding
 
+lateinit var binding : ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setup()
     }
+
+
+    enum class ProviderType{
+        BASIC
+    }
+    private fun setup() {
+        //canviem el títol de l'aplicació
+        title = "Autenticació"
+        //clic al botó inscriu-te
+        binding.button.setOnClickListener {
+            if (binding.editTextTextEmailAddress.text.isNotEmpty() && binding.editTextTextPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.editTextTextEmailAddress.text.toString(),
+                    binding.editTextTextPassword.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        //showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+        //clic al botó Accedir
+        binding.button2.setOnClickListener {
+            if (binding.editTextTextEmailAddress.text.isNotEmpty() && binding.editTextTextPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.editTextTextEmailAddress.text.toString(),
+                    binding.editTextTextPassword.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        //showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    //falla la autenticació d'usuaris
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("S'ha produït un error autenticant l'usuari!")
+        builder.setPositiveButton("Acceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
+    //l'autenticació funciona correctament
+    /*private fun showHome(email: String, provider: ProviderType){
+        val homeIntent = Intent(this, activity_mapa::class.java).apply{
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }*/
 }
